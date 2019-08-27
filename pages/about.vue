d9a099e5446f0287a793ea04cd4cc3debe2c6a77
Jayziv <jaynicolsmith@gmail.com>
2019-08-27 21:30:23 +0100

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<!-- pages/about.vue -->
+
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">About Me</h1>
+      <p>
+        My name is Jay Alexander Nicol-smith aka (Jina), I am a Web Designer / Developer based in Normanton, England. I create websites for those who can't with a no bs mentality :).
+      </p>
+    </div>
+  </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<!-- pages/index.vue -->
+
+<template>
+  <section class="section is-large">
+    <div class="container has-text-centered">
+      <h1 class="title">Hi, I'm Jay Nicol-Smith</h1>
+      <h2 class="subtitle">a Web Developer based in Normanton, England.</h2>
+    </div>
+  </section>
+</template>

afda37aca5d736af5fdf0966b3bc5133294020d5
Jayziv <jaynicolsmith@gmail.com>
2019-09-02 23:30:00 +0100

    LineWrap

--- pages/projects.vue
+++ pages/projects.vue
@@ -4,9 +4,28 @@
   <section class="section is-medium">
     <div class="container has-text-centered">
       <h1 class="title">My Projects</h1>
+
+      <p>This portfolio is my first project built with <span style="color:#41B883;font-weight:bold;">Vue</span>, <span style="color:#35495E;font-weight:bold;">Nuxt</span> and <span style="color:#00D1B2;font-weight:bold;">Bulma</span>.</p>
+      
+      <br>
       <p>
         Some of my projects can be found on <a href="https://github.com/Jayziv" target="_blank">GitHub</a>.
       </p>
     </div>
   </section>
 </template>
+
+<style>
+.msg {
+    transition: all .3s ease;
+    height: 30px;
+    padding: 10px;
+    background-color: #eee;
+    overflow: hidden;
+}
+.msg.v-enter, .msg.v-leave {
+    height: 0;
+    padding: 0 10px;
+    opacity: 0;
+}
+</style>

e3b3b31fa77a9efc8365595d02946d61aed106c1
Jayziv <jaynicolsmith@gmail.com>
2019-09-22 16:44:23 +0100

    Wording Changes

--- pages/about.vue
+++ pages/about.vue
@@ -5,7 +5,7 @@
     <div class="container has-text-centered">
       <h1 class="title">About Me</h1>
       <p>
-        My name is Jay Alexander Nicol-smith aka (Jina), I&nbsp;am&nbsp;a&nbsp;Web Designer / Developer based in Normanton, England.<br><br>I create websites for those who can't with a no bs mentality :).
+        My name is Jay Alexander Nicol-smith, I&nbsp;am&nbsp;a&nbsp;Web Designer / Developer based in Normanton, England.<br><br>I create websites for those who can't with a no bs mentality :).
       </p>
     </div>
   </section>
--- pages/projects.vue
+++ pages/projects.vue
@@ -7,6 +7,7 @@
 
       <p>This portfolio is my first project built with <span style="color:#41B883;font-weight:bold;">Vue</span>, <span style="color:#35495E;font-weight:bold;">Nuxt</span> and <span style="color:#00D1B2;font-weight:bold;">Bulma</span>.</p>
       
+      <p>My current focus is creating modular sections of an e-commerce site.
       <br>
       <p>
         Some of my projects can be found on <a href="https://github.com/Jayziv" target="_blank">GitHub</a>.
